6b30feec0d58944a14bcded10754458fb45dbba9
The Badboy <117584284+THE-BADBOY@users.noreply.github.com>
2023-01-15 11:10:36 +0600

    Add files via upload

---
+++ SetUp.sh
@@ -0,0 +1,196 @@
+echo 
+echo
+
+
+echo "Wait For All Package Download."
+
+echo
+echo
+echo
+
+
+
+pkg update
+
+pkg upgrade
+
+pkg autoclean
+
+pkg clean
+
+termux-change-repo
+
+pkg install curl
+
+pkg install git
+
+pkg install wget
+
+pkg install zip
+
+pkg install unzip
+
+pkg Install python
+
+pkg install python2
+
+pkg install python3
+
+pkg install nodejs
+
+pkg install jq
+
+pkg install libxml2-utils
+
+pkg install grep
+
+pkg install bc
+
+pkg install htop
+
+pkg install figlet
+
+pkg install httping
+
+pkg install dnsutils
+
+pkg install openssh
+
+pkg install ffmpeg
+
+curl -sL https://gist.githubusercontent.com/mskian/6ea9c2b32d5f41867e7cafc88d1b26d5/raw/youtube-dl.sh | bash
+
+pkg install php
+
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/data/data/com.termux/files/usr/bin --filename=composer
+
+pkg install nano
+
+pkg install zsh
+
+pip install --upgrade pip setuptools
+
+pip install --upgrade httpie
+
+pip install -U requests[socks]
+
+pip install requests
+
+pkg install clang
+
+pkg install libffi
+
+pkg install openssl
+
+pip install ddgr
+
+pkg install screenfetch
+
+pkg install w3m
+
+pkg install cowsay
+
+pkg install perl
+
+pkg install ruby
+
+pkg install rust
+
+pkg install proot
+
+pkg install termux-exec
+
+pkg install termux-api
+
+pkg update
+
+pkg upgrade
+
+
+termux-setup-storage
+
+cd .termux
+
+nano termux.properties
+
+termux-upgrade-repo
+
+apt update
+
+apt upgrade
+pip install astroid 
+
+pip install autopep8
+
+pip install certifi
+
+pip install chardet
+
+pip install colorama 
+
+pip install future
+
+pip install idna 
+
+pip install isort 
+
+pip install lazy-object-proxy
+
+pip install lolcat 
+
+pip install mccabe  
+
+pip install Pillow 
+
+pip install pilo 
+
+pip install pip
+
+pip install pycodestyle 
+
+pip install pyfiglet
+
+pip install pylint
+
+pip install requests 
+
+pip install setuptools
+
+pip install six
+
+pip install termcolor
+
+pip install toml
+
+pip install urllib3
+
+pip install wheel
+
+pip install wrapt
+
+pip install youtube-dl
+
+pip install mechanize 
+
+apt install ruby -y
+
+apt install openssh -y
+
+apt install wget -y
+
+apt install curl -y
+apt install proot -y
+
+
+figlet -f slant 'THE-BADBOY-CYBER' |lolcat
+echo
+echo
+echo 
+figlet -f slant 'WELLCOME' |lolcat
+echo
+echo
+echo
+
+echo "Now Your Termux Is Ready To Run," |lolcat
+
+#Remake By THE-BADBOY
